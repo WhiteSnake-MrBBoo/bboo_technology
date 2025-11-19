b3b36ecfcea17376b18dbb1ea4f05c431af7214d
WhiteSnake-MrBBoo <mrbulsapabb@gmail.com>
2025-11-19 17:27:09 +0900

    기본 셋팅

---
+++ src/main/java/com/example/bboo_technology/BbooTechnologyApplication.java
@@ -0,0 +1,13 @@
+package com.example.bboo_technology;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbooTechnologyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BbooTechnologyApplication.class, args);
+    }
+
+}
